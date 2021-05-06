914a8e2fd6ab450a76de8221227cccd46bf37d4e
mail2abhisheksen <mail2abhisheksen@gmail.com>
2021-05-06 19:18:40 +0530

    Payment Service

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -1,10 +1,33 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import {tap, catchError } from "rxjs/operators"
+import { IOrder } from '../models/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
 
+  orderApi: string="http://localhost:8090/Order/getOrderDetails/"
   constructor(private httpClient: HttpClient) { }
+
+  public getOrderDetails(id: number): Observable<IOrder>{
+    return this.httpClient.get<IOrder>(`${this.orderApi}${id}`).pipe(
+      tap(data => console.log('All', JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse){
+    let errorMessage='';
+    if(err.error instanceof ErrorEvent){
+      errorMessage=`An error occured: ${err.error.message}`;
+    }
+    else{
+      errorMessage=`Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
 }
