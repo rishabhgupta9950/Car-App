01b87b0caca667b43b82b3c35db9557ec412113a
nikhilnichit <nichitnikhil12@gmail.com>
2021-05-09 11:08:50 +0530

    Push module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
@@ -21,6 +20,16 @@ import { CarDataComponent } from './car-data/car-data.component';
 
 import { OrderDetailsComponent } from './order-details/order-details.component';
 import { from } from 'rxjs';
+import { RegisterComponent } from './register/register.component';
+import { PaymentAdminComponent } from './payment/payment.admin.component';
+import { AdminDashboardComponent } from './admin/admin.dashboard.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { CarDeleteComponent } from './car-delete/car-delete.component';
+import { CarUpdateComponent } from './car-update/car-update.component';
+import { CartDetailComponent } from './cart-detail/cart-detail.component';
+import { AppointmentAdminComponent } from './appointment/appointment.admin.component';
+import { BillDetailsComponent } from './bill-details/bill-details/bill-details.component';
 
 
 
@@ -29,33 +38,58 @@ import { from } from 'rxjs';
     AppComponent,
     CustomerComponent,
     AdminComponent,
+    AppointmentAdminComponent,
+    AdminDashboardComponent,
     CarComponent,
     AppointmentComponent,
+    AppointmentAdminComponent,
     PaymentComponent,
+    PaymentAdminComponent,
     OrderComponent,
     LoginComponent,
     HeaderComponent,
     FooterComponent,
     HomeComponent,
     CarDataComponent,
-    OrderDetailsComponent
+    OrderDetailsComponent,
+    CarDeleteComponent,
+    CarUpdateComponent,
+    RegisterComponent,
+    CartDetailComponent,
+    BillDetailsComponent
+
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       {path:'home', component: HomeComponent},
       {path:'appointment', component: AppointmentComponent},
+      {path:'appointment-admin', component: AppointmentAdminComponent},
+
+      // {path:'carRegister', component: CarComponent},
+      // {path:'deleteCar', component: CarDeleteComponent},
+      {path:'products', component: CarDataComponent},
+      {path:'carUpdate', component: CarUpdateComponent},
+
       {path:'carRegister', component: CarComponent},
       {path:'products', component: CarDataComponent},
+
       {path:'login', component: LoginComponent},
       {path:'order', component: OrderComponent},
+      {path:'cart', component: CartDetailComponent},
       {path:'order-details', component: OrderDetailsComponent},
-      {path:'register', component: LoginComponent},
+      {path:'register', component: RegisterComponent},
       {path:'payment', component: PaymentComponent},
+      {path:'bill-details', component: BillDetailsComponent},
+      {path:'payment-admin', component: PaymentAdminComponent},
+      {path:'admin', component: AdminComponent},
+      {path:'admin-dashboard', component: AdminDashboardComponent},
+      {path:'customer', component: CustomerComponent},
       {path: '', redirectTo: 'home', pathMatch: 'full'},
       {path: '**', redirectTo: 'home', pathMatch: 'full'}
     ])
