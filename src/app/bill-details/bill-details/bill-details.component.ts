efa750bf91efb32dcdc845c5ec39ddf57ec6bd64
nikhilnichit <nichitnikhil12@gmail.com>
2021-05-10 23:33:32 +0530

    Final

--- src/app/bill-details/bill-details/bill-details.component.ts
+++ src/app/bill-details/bill-details/bill-details.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 import { IOrder } from 'src/app/models/order';
 import { OrderService } from 'src/app/services/order.service';
 
@@ -16,11 +15,11 @@ export class BillDetailsComponent implements OnInit {
   cgst: number = 0;
   amount: number = 0;
 
-  constructor(private orderService: OrderService, private router:Router, private route: ActivatedRoute) {
+  constructor(private orderService: OrderService, private router:Router) {
     console.log(this.router.getCurrentNavigation().extras.state.id); 
-    // this.route.data.subscribe((value) => (console.log(value)));
+    
     let bill = this.orderService.getOrderDetails(this.router.getCurrentNavigation().extras.state.id).subscribe({
-    //   // let bill = this.orderService.getBill(this.router.getCurrentNavigation().extras.state.id).subscribe({
+    
         next: order =>{
           this.order = order;
           console.log(order.billingDate);
@@ -35,20 +34,7 @@ export class BillDetailsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // console.log(this.router.getCurrentNavigation().extras.state);
-    // let bill = this.orderService.getBill(this.sub).subscribe({
-    // let bill = this.orderService.getBill(this.router.getCurrentNavigation().extras.state.id).subscribe({
-      //let bill = this.route.data.subscribe(v=>console.log(v));
-    //   next: order =>{
-    //     this.order = order;
-    //     for(let i=0;i<this.order.car.length;i++){
-    //       this.total+=this.order.car[i].price;
-    //     }
-    //     this.sgst = this.total*0.09;
-    //     this.cgst = this.total*0.09;
-    //     this.amount=this.total+this.sgst+this.cgst;
-    //   }
-    // });
+    
   }
 
 }
